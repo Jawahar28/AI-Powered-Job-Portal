2ca84369026812e4dbd3b9b121a84ba5f5b5eb72
Jawahar28 <jawaharn246@gmail.com>
2026-06-29 23:33:58 +0530

    Created Initial Company Model and database migrations

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# ADMIN PANEL: 
+# USERNAME: django123
+# Password : Django@28
