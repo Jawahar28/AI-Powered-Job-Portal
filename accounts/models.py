7d57111812d0f322f5a36aaf98bc6ee322d047c4
Jawahar28 <jawaharn246@gmail.com>
2026-07-02 23:12:53 +0530

    Added Meta class and Choices field

--- accounts/models.py
+++ accounts/models.py
@@ -3,3 +3,23 @@ from django.db import models
 # Create your models here.
 class Company(models.Model):
     name = models.CharField(max_length= 100)
+
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
+class Job(models.Model):
+
+    class Status(models.TextChoices):
+        OPEN = "O", "Open"
+        CLOSED = "C", "Closed"
+        DRAFT = "D", "Draft"
+    title = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=1, choices=Status.choices, default=Status.OPEN
+    )
