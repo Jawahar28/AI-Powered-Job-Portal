2dca48ff76ec2f2ce31bf63d5cf7c7adbdfbafb4
Jawahar28 <jawaharn246@gmail.com>
2026-07-14 23:27:20 +0530

    Django's  Request-Response Cycle

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def job_list(request):
+    # return HttpResponse("Welcome to AI-Powered Job Portal")
+    return render(request, "jobs/job_list.html")
