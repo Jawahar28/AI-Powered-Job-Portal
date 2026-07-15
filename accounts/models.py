805005b68a210662fc64e243f6aad7879b656bb4
Jawahar28 <jawaharn246@gmail.com>
2026-07-15 23:58:20 +0530

    View, Templates

--- accounts/models.py
+++ accounts/models.py
@@ -1,33 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Company(models.Model):
-    name = models.CharField(max_length= 100)
 
-    
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ['name']
-        verbose_name = "Company"
-        verbose_name_plural = "Companies"
-
-class Job(models.Model):
-
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    
-    class Status(models.TextChoices):
-        OPEN = "O", "Open"
-        CLOSED = "C", "Closed"
-        DRAFT = "D", "Draft"
-
-    
-    status = models.CharField(
-        max_length=1, choices=Status.choices, default=Status.OPEN
-    )
-
-    def __str__(self):
-        return self.title
     
--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Job
 
 # Create your views here.
 def job_list(request):
     # return HttpResponse("Welcome to AI-Powered Job Portal")
-    return render(request, "jobs/job_list.html")
+    jobs = Job.objects.all()
+
+    return render(request,
+                "jobs/job_list.html",
+                {
+                    'jobs': jobs
+                }
+            )
