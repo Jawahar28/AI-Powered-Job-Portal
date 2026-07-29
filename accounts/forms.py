bbcf9de4e79c2150307700c4f41ef9b5f709fd4c
Jawahar28 <jawaharn246@gmail.com>
2026-07-29 11:22:44 +0530

    Login Page Created

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,7 +14,23 @@ class RegisterForm(UserCreationForm):
             'password2',
         ]
 
-class Login(AuthenticationForm):
-    
+class LoginForm(AuthenticationForm):
 
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Username"
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Password"
+            }
+        )
+    )
 
