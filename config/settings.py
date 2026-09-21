a0d86a86ec02c8bb3f8cae4b6ffc66b3ad775d5a
Jawahar28 <jawaharn246@gmail.com>
2026-09-21 22:58:40 +0530

    Notifications added to JOBCode

--- config/settings.py
+++ config/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
+                'jobs.context_processors.notification_count',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
